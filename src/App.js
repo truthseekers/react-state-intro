844ff124268aa796b68b5c5ab90f75d3c9f70717
John Curry <john@truthseekers.io>
2020-10-17 07:11:35 -0700

    controlled forms intro complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [textField, setTextField] = useState("");
+  const [firstName, setFirstName] = useState("");
 
   const increaseCount = () => {
     setCount(count + 1);
@@ -13,12 +15,38 @@ function App() {
     setCount(count - 1);
   };
 
+  const handleNameFieldChange = (event) => {
+    setTextField(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFirstName(textField);
+    setTextField("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h3>The current count is: {count}</h3>
         <button onClick={increaseCount}>count + 1</button>
         <button onClick={decreaseCount}>count - 1</button>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              value={textField}
+              onChange={handleNameFieldChange}
+              name="name"
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <h3>Hello {firstName}! How are you today?</h3>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
